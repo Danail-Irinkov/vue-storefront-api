58c4b58a9b3c6800fa42781e17a082b8a85ef27a
andrzejewsky <vox3r69@gmail.com>
2019-10-06 21:09:58 +0200

    update resolvers

--- src/graphql/elasticsearch/catalog/resolver.js
+++ src/graphql/elasticsearch/catalog/resolver.js
@@ -3,6 +3,7 @@ import client from '../client';
 import { buildQuery } from '../queryBuilder';
 import esResultsProcessor from './processor'
 import { getIndexName } from '../mapping'
+import { adjustQuery } from './../../../lib/elastic'
 
 const resolver = {
   Query: {
@@ -12,7 +13,12 @@ const resolver = {
 };
 
 async function list (filter, sort, currentPage, pageSize, search, context, rootValue, _sourceInclude, _sourceExclude) {
-  const { req, res } = context;
+  let _req = {
+    query: {
+      _source_exclude: _sourceExclude,
+      _source_include: _sourceInclude
+    }
+  }
 
   let query = buildQuery({
     filter: filter,
@@ -23,32 +29,31 @@ async function list (filter, sort, currentPage, pageSize, search, context, rootV
     type: 'product'
   });
 
-  let esIndex = getIndexName(req.url)
+  let esIndex = getIndexName(context.req.url)
 
-  let esResponse = await client.search({
+  let esResponse = await client.search(adjustQuery({
     index: esIndex,
-    type: config.elasticsearch.indexTypes[0],
     body: query,
     _sourceInclude,
     _sourceExclude
-  });
+  }, 'product', config));
 
-  if (esResponse && esResponse.hits && esResponse.hits.hits) {
+  if (esResponse && esResponse.body.hits && esResponse.body.hits.hits) {
     // process response result (caluclate taxes etc...)
-    esResponse.hits.hits = await esResultsProcessor(esResponse, config.elasticsearch.indexTypes[0], esIndex, req, res);
+    esResponse.body.hits.hits = await esResultsProcessor(esResponse, _req, config.elasticsearch.indexTypes[0], esIndex);
   }
 
   let response = {}
 
   // Process hits
   response.items = []
-  esResponse.hits.hits.forEach(hit => {
+  esResponse.body.hits.hits.forEach(hit => {
     let item = hit._source
     item._score = hit._score
     response.items.push(item)
   });
 
-  response.total_count = esResponse.hits.total
+  response.total_count = esResponse.body.hits.total
 
   // Process sort
   let sortOptions = []
