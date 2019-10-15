c05aeb2f524c51eeece12ac3fea7a6c113b61fc6
ResuBaka <resubaka@gmail.com>
2019-10-15 15:43:48 +0200

    Fixed graphql resolvers where the _source_include or _source_include has not worked anymore

--- src/graphql/elasticsearch/catalog/resolver.js
+++ src/graphql/elasticsearch/catalog/resolver.js
@@ -3,7 +3,6 @@ import client from '../client';
 import { buildQuery } from '../queryBuilder';
 import esResultsProcessor from './processor'
 import { getIndexName } from '../mapping'
-import { adjustQuery } from './../../../lib/elastic'
 
 const resolver = {
   Query: {
@@ -12,11 +11,11 @@ const resolver = {
   }
 };
 
-async function list (filter, sort, currentPage, pageSize, search, context, rootValue, _sourceInclude, _sourceExclude) {
+async function list (filter, sort, currentPage, pageSize, search, context, rootValue, _source_include, _source_exclude) {
   let _req = {
     query: {
-      _source_exclude: _sourceExclude,
-      _source_include: _sourceInclude
+      _source_exclude,
+      _source_include
     }
   }
 
@@ -31,29 +30,32 @@ async function list (filter, sort, currentPage, pageSize, search, context, rootV
 
   let esIndex = getIndexName(context.req.url)
 
-  let esResponse = await client.search(adjustQuery({
+  let esResponse = await client.search({
     index: esIndex,
+    type: config.elasticsearch.indexTypes[0],
     body: query,
-    _sourceInclude,
-    _sourceExclude
-  }, 'product', config));
+    _source_include,
+    _source_exclude
+  });
 
-  if (esResponse && esResponse.body.hits && esResponse.body.hits.hits) {
+  const { body } = esResponse
+
+  if (body && body.hits && body.hits.hits) {
     // process response result (caluclate taxes etc...)
-    esResponse.body.hits.hits = await esResultsProcessor(esResponse, _req, config.elasticsearch.indexTypes[0], esIndex);
+    body.hits.hits = await esResultsProcessor(body, _req, config.elasticsearch.indexTypes[0], esIndex);
   }
 
   let response = {}
 
   // Process hits
   response.items = []
-  esResponse.body.hits.hits.forEach(hit => {
+  body.hits.hits.forEach(hit => {
     let item = hit._source
     item._score = hit._score
     response.items.push(item)
   });
 
-  response.total_count = esResponse.body.hits.total
+  response.total_count = body.hits.total
 
   // Process sort
   let sortOptions = []
@@ -66,7 +68,7 @@ async function list (filter, sort, currentPage, pageSize, search, context, rootV
     )
   }
 
-  response.aggregations = esResponse.aggregations
+  response.aggregations = body.aggregations
   response.sort_fields = {}
   if (sortOptions.length > 0) {
     response.sort_fields.options = sortOptions
--- src/graphql/elasticsearch/category/resolver.js
+++ src/graphql/elasticsearch/category/resolver.js
@@ -1,23 +1,23 @@
 import config from 'config';
 import client from '../client';
-import { buildQuery } from '../queryBuilder'
+import { buildQuery } from '../queryBuilder';
 import { getIndexName } from '../mapping'
-import { adjustQuery } from './../../../lib/elastic'
 
-async function list (search, filter, currentPage, pageSize = 200, sort, context, rootValue, _sourceIncludes) {
+async function list (search, filter, currentPage, pageSize = 200, sort, context, rootValue, _source_include) {
   let query = buildQuery({ search, filter, currentPage, pageSize, sort, type: 'category' });
 
-  if (_sourceIncludes === undefined) {
-    _sourceIncludes = config.entities.category.includeFields
+  if (_source_include === undefined) {
+    _source_include = config.entities.category.includeFields
   }
 
-  const response = await client.search(adjustQuery({
+  const response = await client.search({
     index: getIndexName(context.req.url),
+    type: config.elasticsearch.indexTypes[1],
     body: query,
-    _sourceIncludes
-  }, 'category', config));
+    _source_include
+  });
 
-  return response.body;
+  return response;
 }
 
 const resolver = {
