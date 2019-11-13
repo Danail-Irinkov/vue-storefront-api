c2a43b484458f0a8edb18c5d740dbc323576d337
Dan <d.irinkovn1@gmail.com>
2019-11-13 23:47:45 +0200

    Trying to fix Build process

--- docker/vue-storefront-api/vue-storefront-api.sh
+++ docker/vue-storefront-api/vue-storefront-api.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 set -e
 
-#copy repo_configs to PersistentDisk
-rm -f ./config_repo/production.json
-cp -a -r -f config_repo/. config
-
 yarn install || exit $?
 
-#if [ "$VS_ENV" = 'dev' ]; then
-#  yarn dev
-#else
-#  npm install pm2 -g
-#  npm run build
-  yarn start2
-#fi
+if [ "$VS_ENV" = 'dev' ]; then
+  yarn dev
+else
+  yarn build || exit $?
+  yarn start
+fi
