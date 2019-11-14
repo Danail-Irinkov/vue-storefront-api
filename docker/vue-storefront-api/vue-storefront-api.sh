014830ee5b051be377c57e1751349f38978c4421
Dan <d.irinkovn1@gmail.com>
2019-11-14 23:17:32 +0200

    created persistent volume for the config folder

--- docker/vue-storefront-api/vue-storefront-api.sh
+++ docker/vue-storefront-api/vue-storefront-api.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+#copy repo_configs to PersistentDisk
+cp -a /config/. /config2/
+ls -la config2
+
+rm /config_repo/production.js
+cp -a -T /config_repo/. /config/
+ls -la config
+
 yarn install || exit $?
 
 if [ "$VS_ENV" = 'dev' ]; then
