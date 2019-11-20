24e93e8842f6c3e895ef4ba7d0444ea878dedf3c
Dan <d.irinkovn1@gmail.com>
2019-11-20 02:20:08 +0200

    fixing bad API endpoint

--- src/api/extensions/procc/index.js
+++ src/api/extensions/procc/index.js
@@ -131,17 +131,19 @@ function (_err, _res, _resBody) {
     return apiStatus(res, 200);
   });
   mcApi.post('/backup-config', (req, res) => {
-    request({
-        //store url with custom function
-        uri:'http://'+config.vsf.host+':'+config.vsf.port+'/backup-config',
-        method:'POST',
-        body: req.body,
-        json: true
-      },
-      function (_err, _res, _resBody) {
-        let backupConfigFiles = {"vsf_config_data": _resBody,"vsf_api_config_data": config}
-        return apiStatus(res, backupConfigFiles, 200);
-      })
+    // request({
+    //     //store url with custom function
+    //     uri:'http://'+config.vsf.host+':'+config.vsf.port+'/backup-config',
+    //     method:'POST',
+    //     body: req.body,
+    //     json: true
+    //   },
+    //   function (_err, _res, _resBody) {
+    //     let backupConfigFiles = {"vsf_config_data": _resBody,"vsf_api_config_data": config}
+    //     return apiStatus(res, backupConfigFiles, 200);
+    //   })
+    let backupConfigFiles = {"vsf_api_config_data": config.vsf.host}
+    return apiStatus(res, backupConfigFiles, 200);
   });
 
   mcApi.post('/create-store-index', async (req, res) => {
