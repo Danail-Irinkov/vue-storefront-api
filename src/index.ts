c22a21b0c151687d616279460e608b2f11cea167
Dan <d.irinkovn1@gmail.com>
2019-12-04 02:29:36 +0200

    Fixing Store Checkout Flow Adding missing code

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,10 @@ import resolvers from './graphql/resolvers';
 import typeDefs from './graphql/schema';
 import * as path from 'path'
 
+// Added ProCCAPI to global added by Dan to enable in typescript
+import ProCcApiRaw from './helpers/procc_api'
+(global as any).ProCcAPI = ProCcApiRaw();
+
 // Added by dan-29-11-2019
 const timeout = require('connect-timeout')
 
