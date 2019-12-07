dd0812e3632b07bc48cab8675e4adbad417cff12
Dan <d.irinkovn1@gmail.com>
2019-12-07 03:19:52 +0200

    fixing node_env in production

--- docker/vue-storefront-api/vue-storefront-api.sh
+++ docker/vue-storefront-api/vue-storefront-api.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 set -e
 
+#copy repo_configs to PersistentDisk
+rm -f ./config_repo/production.json
+cp -a -r -f config_repo/. config
+
 yarn install || exit $?
 
 if [ "$VS_ENV" = 'dev' ]; then
   yarn dev
 else
-  yarn start
+  npm install pm2 -g
+  npm run build
+  set NODE_ENV=production
+  yarn startK
 fi
