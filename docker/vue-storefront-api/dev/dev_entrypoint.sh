1d9fad0ebf2cde69442c405e5bf6df5fcac641d3
Dan <d.irinkovn1@gmail.com>
2020-01-03 22:03:58 +0200

    Dev Script working in linux

--- docker/vue-storefront-api/dev/dev_entrypoint.sh
+++ docker/vue-storefront-api/dev/dev_entrypoint.sh
@@ -9,7 +9,6 @@ cp -f config_bin/local_docker.json config/local.json
 
 yarn install
 yarn restore2main
-ls -la config
 yarn mage2vs import --store-code=dev --skip-products=1 --skip-pages=1 --skip-blocks=1
 yarn mage2vs productsdelta --store-code=dev --partitions=1 --partitionSize=20 --skus=DA001,DA002,DA003,DA004,DA005,DA006,DA007,DA008,DA009
 
