d24a186118724aeb0fd46ac3402b927cd760cd3f
Dan <d.irinkovn1@gmail.com>
2020-01-07 22:02:28 +0200

    rest revert changes

--- docker/vue-storefront-api/vue-storefront-api.sh
+++ docker/vue-storefront-api/vue-storefront-api.sh
@@ -5,16 +5,13 @@ set -e
 rm -f ./config_repo/production.json
 cp -a -r -f config_repo/. config
 
-#npm config set scripts-prepend-node-path auto
-npm install -g typescript@3.3.3
-#npm install -g pm2
-
 yarn install || exit $?
+
 #if [ "$VS_ENV" = 'dev' ]; then
 #  yarn dev
 #else
 #  npm install pm2 -g
 #  npm run build
-yarn start2
+  yarn start2 
 #  yarn startK2
 #fi
