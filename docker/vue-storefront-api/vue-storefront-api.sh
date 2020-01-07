5a55731d7faa9462aaa52fb25eaf9c63525fca38
Dan <d.irinkovn1@gmail.com>
2020-01-07 21:23:54 +0200

    fixing kube config

--- docker/vue-storefront-api/vue-storefront-api.sh
+++ docker/vue-storefront-api/vue-storefront-api.sh
@@ -7,7 +7,7 @@ cp -a -r -f config_repo/. config
 
 npm config set scripts-prepend-node-path true
 npm install -g typescript@3.3.3
-npm install -g pm2
+#npm install -g pm2
 
 yarn install || exit $?
 #if [ "$VS_ENV" = 'dev' ]; then
