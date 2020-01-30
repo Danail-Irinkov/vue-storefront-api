dc4d2443e7fa0fde6b0cfc7c14cba3e906a1cbed
Dan <d.irinkovn1@gmail.com>
2020-01-30 22:38:55 +0200

    exporting secrets to private repo

---
+++ config/jwt.js
@@ -0,0 +1,7 @@
+export default {
+  private_key: '-----BEGIN RSA PRIVATE KEY-----\n' +
+  'rtghj356uwjuw56u5e67kie567o6r478or78;lr78or6789i4678i46A1KoS8kVw\n' +
+  '33c2q53g245g2354ewfg6tw45ygwrth678l5678o678k467i46783567u67KE4qW\n' +
+  'FaFp+2354235625y24tg2456y245yhwrth245yy54w==\n' +
+  '-----END RSA PRIVATE KEY-----'
+}
